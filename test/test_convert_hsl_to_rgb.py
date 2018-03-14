1643d46f371346ef832468b217ad8ade4ab64da1
Anthony Duren <amduren@gmail.com>
2018-03-14 10:07:14 -0500

    Add more tests for utilities functions

--- test/test_convert_hsl_to_rgb.py
+++ test/test_convert_hsl_to_rgb.py
@@ -8,32 +8,32 @@ class HslToRgbTests(unittest.TestCase):
     def test_hue_value_less_than_0_returns_message(self):
         color = ConvertToRgb()
         result = color.convert_hsl_to_rgb(-50, 0.50, 0.25)
-        self.assertEqual('Hue must be between 0 and 360.', result)
+        self.assertEqual(result, 'Hue must be between 0 and 360.')
 
     def test_hue_value_greater_than_360_returns_message(self):
         color = ConvertToRgb()
         result = color.convert_hsl_to_rgb(450, 0.50, 0.25)
-        self.assertEqual('Hue must be between 0 and 360.', result)
+        self.assertEqual(result, 'Hue must be between 0 and 360.')
 
     def test_saturation_value_less_than_0_returns_message(self):
         color = ConvertToRgb()
         result = color.convert_hsl_to_rgb(180, -0.50, 0.25)
-        self.assertEqual('Saturation must be between 0 and 1.', result)
+        self.assertEqual(result, 'Saturation must be between 0 and 1.')
 
     def test_saturation_value_greater_than_1_returns_message(self):
         color = ConvertToRgb()
         result = color.convert_hsl_to_rgb(180, 1.50, 0.25)
-        self.assertEqual('Saturation must be between 0 and 1.', result)
+        self.assertEqual(result, 'Saturation must be between 0 and 1.')
         
     def test_luminance_value_less_than_0_returns_message(self):
         color = ConvertToRgb()
         result = color.convert_hsl_to_rgb(180, 0.50, -0.25)
-        self.assertEqual('Luminance must be between 0 and 1.', result)
+        self.assertEqual(result, 'Luminance must be between 0 and 1.')
 
     def test_luminance_value_greater_than_1_returns_message(self):
         color = ConvertToRgb()
         result = color.convert_hsl_to_rgb(180, 0.50, 1.25)
-        self.assertEqual('Luminance must be between 0 and 1.', result)
+        self.assertEqual(result, 'Luminance must be between 0 and 1.')
 
     def test_saturation_value_of_0_returns_rgb_list_with_all_values_equal(self):
         color = ConvertToRgb()
@@ -45,6 +45,8 @@ class HslToRgbTests(unittest.TestCase):
         color = ConvertToRgb()
         result = color.convert_hsl_to_rgb(180, 0.00, 0.40)
         self.assertEqual(result[0], 102)    # 0.40 * 255 = 102
+        self.assertEqual(result[1], 102)    # 0.40 * 255 = 102
+        self.assertEqual(result[2], 102)    # 0.40 * 255 = 102
 
     def test_rgb_list_has_length_3(self):
         color = ConvertToRgb()
--- test/test_utilities.py
+++ test/test_utilities.py
@@ -1,21 +1,21 @@
 import unittest
 
-from app.utilities.utilities import map_char_to_int
+from app.utilities.utilities import map_char_to_int, map_int_to_char
 
 
 class UtilitiesTests(unittest.TestCase):
 
     def test_length_string_of_chars_less_than_length_list_of_ints_returns_message(self):
         result = map_char_to_int('a', 'abc', range(5))
-        self.assertEqual('Not enough characters to map to an integer.', result)
+        self.assertEqual(result, 'Not enough characters to map to an integer.')
 
     def test_length_string_of_chars_greater_than_length_list_of_ints_returns_message(self):
         result = map_char_to_int('a', 'abcdef', range(5))
-        self.assertEqual('Too many characters to map to an integer.', result)
+        self.assertEqual(result, 'Too many characters to map to an integer.')
 
     def test_char_not_in_string_of_chars_returns_message(self):
         result = map_char_to_int('z', 'abcde', range(5))
-        self.assertEqual('Character not in string of characters.', result)
+        self.assertEqual(result, 'Character not in string of characters.')
 
     def test_char_in_string_of_chars_returns_int(self):
         result = map_char_to_int('a', 'abc', range(3))
@@ -23,4 +23,24 @@ class UtilitiesTests(unittest.TestCase):
 
     def test_char_in_string_of_chars(self):
         result = map_char_to_int('a', 'abc', range(3))
-        self.assertEqual(0, result)
+        self.assertEqual(result, 0)
+
+    def test_length_list_of_ints_less_than_length_string_of_chars_returns_message(self):
+        result = map_int_to_char(4, range(5), 'abcdefghijklmnopqrstuvwxyz')
+        self.assertEqual(result, 'Not enough integers to map to a character.')
+
+    def test_length_list_of_ints_greater_than_length_string_of_chars_returns_message(self):
+        result = map_int_to_char(1, range(5), 'abc')
+        self.assertEqual(result, 'Too many integers to map to a character.')
+
+    def test_int_not_in_list_of_ints_returns_message(self):
+        result = map_int_to_char(9, range(10, 16), 'abcdef')
+        self.assertEqual(result, 9)     # return integer
+
+    def test_int_in_list_of_ints_returns_char(self):
+        result = map_int_to_char(0, range(3), 'abc')
+        self.assertEqual(type(result), str)     # character should be a string
+
+    def test_int_in_list_of_ints(self):
+        result = map_int_to_char(2, range(1, 4), 'abc')
+        self.assertEqual(result, 'b')
