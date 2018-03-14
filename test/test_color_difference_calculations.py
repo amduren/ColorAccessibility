9a49c4c7ae480d08f77f0be43292eb1ecbb64340
Anthony Duren <amduren@gmail.com>
2018-03-14 07:14:29 -0500

    Update tests

--- test/test_color_difference_calculations.py
+++ test/test_color_difference_calculations.py
@@ -8,30 +8,30 @@ class ColorDifferenceTests(unittest.TestCase):
     def test_color_difference_less_than_500_returns_message(self):
         difference = ColorCalculations()
         result = difference.calculate_color_difference([0, 0, 0], [0, 0, 0])
-        self.assertEqual('Color difference is 0. Color difference must be at least 500 to pass.', result)
+        self.assertEqual('Color difference is 0. Color difference MUST BE AT LEAST 500 to pass the standard.', result)
 
     def test_color_difference_greater_than_500_returns_message(self):
         difference = ColorCalculations()
         result = difference.calculate_color_difference([255, 255, 0], [0, 0, 255])
-        self.assertEqual('Color difference is 765. Color difference is sufficient.', result)
+        self.assertEqual('Color difference is 765. Color difference PASSES the standard.', result)
 
     def test_color_difference_is_500_returns_message(self):
         difference = ColorCalculations()
         result = difference.calculate_color_difference([245, 0, 0], [0, 0, 255])
-        self.assertEqual('Color difference is 500. Color difference is sufficient.', result)
+        self.assertEqual('Color difference is 500. Color difference PASSES the standard.', result)
 
     def test_color_brightness_difference_less_than_125_returns_message(self):
         difference = ColorCalculations()
         result = difference.calculate_color_brightness_difference([0, 0, 0], [0, 0, 0])
-        self.assertEqual('Color brightness difference is 0.0. Color brightness difference must be at least 125 to '
-                         + 'pass.', result)
+        self.assertEqual('Color brightness difference is 0.0. Color brightness difference MUST BE AT LEAST 125 to '
+                         + 'pass the standard.', result)
 
     def test_color_brightness_difference_greater_than_125_returns_message(self):
         difference = ColorCalculations()
         result = difference.calculate_color_brightness_difference([255, 255, 0], [0, 0, 255])
-        self.assertEqual('Color brightness difference is 196.86. Color brightness is sufficient.', result)
+        self.assertEqual('Color brightness difference is 196.86. Color brightness PASSES the standard.', result)
 
     def test_color_brightness_difference_is_125_returns_message(self):
         difference = ColorCalculations()
         result = difference.calculate_color_brightness_difference([254, 133, 0], [0, 0, 254])
-        self.assertEqual('Color brightness difference is 125.06. Color brightness is sufficient.', result)
+        self.assertEqual('Color brightness difference is 125.06. Color brightness PASSES the standard.', result)
