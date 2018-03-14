dcd5ac8e413fa81f7098f49a42543344050cd936
Anthony Duren <amduren@gmail.com>
2018-03-14 11:19:12 -0500

    Add tests for different hex colors

--- test/test_convert_hex_to_rgb.py
+++ test/test_convert_hex_to_rgb.py
@@ -26,3 +26,27 @@ class HexToRgbTests(unittest.TestCase):
         self.assertEqual(type(result[0]), int)
         self.assertEqual(type(result[1]), int)
         self.assertEqual(type(result[2]), int)
+
+    def test_hex_black(self):
+        result = self.color.convert_hex_to_rgb('000000')
+        self.assertEqual(result, [0, 0, 0])
+
+    def test_hex_white(self):
+        result = self.color.convert_hex_to_rgb('#FFFFFF')
+        self.assertEqual(result, [255, 255, 255])
+
+    def test_hex_red(self):
+        result = self.color.convert_hex_to_rgb('FF0000')
+        self.assertEqual(result, [255, 0, 0])
+
+    def test_hex_green(self):
+        result = self.color.convert_hex_to_rgb('#00ff00')
+        self.assertEqual(result, [0, 255, 0])
+
+    def test_hex_blue(self):
+        result = self.color.convert_hex_to_rgb('0000ff')
+        self.assertEqual(result, [0, 0, 255])
+
+    def test_hex_pink(self):
+        result = self.color.convert_hex_to_rgb('#f15d9A')
+        self.assertEqual(result, [241, 93, 154])
